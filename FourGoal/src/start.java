07c1f28014ae8eae75236fcfcd3a50d7b4808043
macmeng <vipmengfeng@163.com>
2016-08-26 21:52:15 +0800

    重要更新，任意连续三场结果相同，对比数据增至50轮，相同范围改为2-5，初始订单改为全部

--- FourGoal/src/start.java
+++ FourGoal/src/start.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 
 public class start {
-	 public static String[] str1 = { "3","2","0"};
-	 public static String[] str2 = {  "1","0"};
-	 public static String[] str3 = { "3","2", "1","0"};
-	 public static String[] str4 = {  "1","0"};
-	 public static String[] str5 = { "2", "1","0"};
-	 public static String[] str6 = { "3","2", "1"};
-	 public static String[] str7 = { "3","2", "1","0"};
-	 public static String[] str8 = { "2", "1" };
+	 public static String[] str1 = { "3","2","1","0"};
+	 public static String[] str2 = { "3","2","1","0"};
+	 public static String[] str3 = { "3","2","1","0"};
+	 public static String[] str4 = { "3","2","1","0"};
+	 public static String[] str5 = { "3","2","1","0"};
+	 public static String[] str6 = { "3","2","1","0"};
+	 public static String[] str7 = { "3","2","1","0"};
+	 public static String[] str8 = { "3","2","1","0"};
 	 static ArrayList<String> dataArray = new ArrayList<String> ();
 	 static ArrayList<String> dataArrays = new ArrayList<String> ();
 	 static ArrayList<String> datafile = new ArrayList<String> ();
@@ -34,7 +34,7 @@ public static void main(String[] args){
 	datafile=ms.ReadDatas("/Users/meng/Documents/fourgoal/FourGoal/src/four.txt", datafile);
 	datafile2=datafile;
 	ms.s(datafile.size());
-	lishi();
+	//lishi();
 	//断点范围5-6
 	for(int i=0;i<dataArray.size();i++){
 		int dd=ms.dd(dataArray.get(i));
@@ -44,28 +44,28 @@ public static void main(String[] args){
 	}
 	put();
 	
-	//test();
+	test();
 	nums("3",1,2);
 	nums("2",2,4);
 	nums("1",2,4);
 	nums("0",1,3);
-	contains(4,5,6,"0");//456场包含0
-	contains(6,7,8,"2");//678场包含2
+	contains(4,5,6,"0","0");//456场包含0
+	contains(6,7,8,"2","3");//678场包含2或3
 	lst();//上轮结果相同位置相同数字 个数范围1-3
-	int arr[]={2,8};//包含奇数列
+	//int arr[]={2,8};//包含奇数列
 	int arrs[]={1,5,6};//包含偶数列
 	//jiou(arr,1);
-	//jiou(arrs,0);
+	jiou(arrs,0);
 //	ping(2,3);
 //	ping(4,5);
-	ping(1,2);
-	ping(7,8);
+	ping(1,2);//平局排除
+	//ping(7,8);
 	
 	sp(3,4);
 	sp(1,2);//胜平锁定
 	//sp(7,8);
 	left_e();//斜向相同数字范围1-3
-	last_f();//最近四轮任意三场重复排除
+	last_f();//最近50轮任意三场重复范围2-5
 	//lishipc();//历史排除
 	
 	
@@ -219,13 +219,15 @@ private static void lishipc(){
 	ms.s("历史排除后");
 	result(1);
 }//任意三场包含
-private  static void contains(int a,int b,int c,String d){
+private  static void contains(int a,int b,int c,String d,String e){
 	dataArrays.clear();
 	for(int i=0;i<dataArray.size();i++){
 		String a1=ms.cutNumber(dataArray.get(i), a);
 		String a2=ms.cutNumber(dataArray.get(i), b);
 		String a3=ms.cutNumber(dataArray.get(i), c);
 		if(a1.equals(d)||a2.equals(d)||a3.equals(d)){
+			dataArrays.add(dataArray.get(i));
+		}else if(a1.equals(e)||a2.equals(e)||a3.equals(e)){
 			dataArrays.add(dataArray.get(i));
 		}
 		
@@ -260,7 +262,7 @@ private static void last_f(){
 		String str4=dataArray.get(i).substring(3,6);
 		String str5=dataArray.get(i).substring(4,7);
 		//String str6=datafile.get(i).substring(5,7);
-		for(int j=i+1;j<i+10;j++){
+		for(int j=i+1;j<i+50;j++){
 			
 			String strs1=datafile.get(j).substring(0,3);
 			String strs2=datafile.get(j).substring(1,4);
@@ -274,7 +276,7 @@ private static void last_f(){
 				
 			}
 		}
-		if(count==0||count==1){
+		if(count>1&&count<6){
 			dataArrays.add(dataArray.get(i));
 		}
 		
@@ -287,7 +289,7 @@ private static void last_f(){
 //
 private static void test(){
 	
-	for(int i=0;i<datafile.size()-12;i++){
+	for(int i=0;i<datafile.size()-52;i++){
 		int count=0;
 		//ms.s(datafile.get(i).length());
 		String str1=datafile.get(i).substring(0, 3);
@@ -296,7 +298,7 @@ private static void test(){
 		String str4=datafile.get(i).substring(3,6);
 		String str5=datafile.get(i).substring(4,7);
 		//String str6=datafile.get(i).substring(5,7);
-		for(int j=i+1;j<i+10;j++){
+		for(int j=i+1;j<i+50;j++){
 			
 			String strs1=datafile.get(j).substring(0,3);
 			String strs2=datafile.get(j).substring(1,4);
